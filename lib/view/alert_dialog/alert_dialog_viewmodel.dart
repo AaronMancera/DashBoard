922387dfdfc113ffcf4057623b3e501de4bbb7b1
Aarón <aaron.mancera.cantador.alu@iesjulioverne.es>
2023-04-19 09:50:48 +0200

    [ Cambio ]
    Creacion de un widget que va a ser el encargado de mostrar los filed/inputs y selectores de personalizacion

--- lib/view/alert_dialog/alert_dialog_viewmodel.dart
+++ lib/view/alert_dialog/alert_dialog_viewmodel.dart
@@ -1,4 +1,6 @@
 import 'package:stacked/stacked.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
 
 class AlertDialogViewModel extends BaseViewModel {
   //Todos los nombres/identificadores de widgets estan aqui
@@ -23,4 +25,84 @@ class AlertDialogViewModel extends BaseViewModel {
   void restantes(List<String> valores) {
     widgestDisponibles.removeWhere((element) => valores.contains(element));
   }
+  //TODO: Customizacion de wigets
+  //QR
+  String urlQr = "";
+  //Cartesian - Circular - FunnelChart - PyramidChart
+  final List<ChartData> valoresChart = [];
+  //Date-Range
+  final Map<String, DateRangePickerView> rangosDias = {
+    "Century": DateRangePickerView.century,
+    "Decade": DateRangePickerView.decade,
+    "Year": DateRangePickerView.year,
+    "Month": DateRangePickerView.month,
+  };
+  bool enableMultiView = false;
+  //LinearGauge - RadialGauge
+  int minimo = 0;
+  int maximo = 0;
+  //LinearGauge
+  List<LinearGaugeOrientation> orientacion=[
+    LinearGaugeOrientation.horizontal,LinearGaugeOrientation.vertical
+  ];
+  List<LinearGaugeRange> rangosLinear=[
+    
+  ];
+  bool markerPointer=false;
+  int valorPointer=0;
+  bool linearBarPointer=false;
+  int valorBarPinter=0;
+
+  //RadialGauge
+
+  //Actualizacion del qr
+  void updateQr(String value) {
+    urlQr = value;
+    notifyListeners();
+  }
+
+  //Actualizacion de los valores para los chart
+  void updateValoresChart(ChartData chartData) {
+    valoresChart.add(chartData);
+    notifyListeners();
+  }
+
+  //Actualizar el multiview
+  void updateEnableMultiView(bool opcion) {
+    enableMultiView = opcion;
+    notifyListeners();
+  }
+  //NOTE: Actualizar configuracion para el LinearGauge
+  //Añade rango personalizado con colores y todo - LinearGaugeRange(startValue: 0, endValue: 50, color: Colors.green),
+  void updateLinearGaugeRange(LinearGaugeRange linearGaugeRange){
+    rangosLinear.add(linearGaugeRange);
+    notifyListeners();
+  }
+  //Activar la opcion de que aparezca el pmarkerpointer
+  void updateMarkerPointer(bool opcion){
+    markerPointer=opcion;
+    notifyListeners();
+  }
+  //Asignaer el valor
+  void updateValorPointer(valor){
+    valorPointer=valor;
+    notifyListeners();
+  }
+  //Activar la opcion de que aparezca un bar coloreado
+  void updateLinearBarPointer(bool opcion){
+    linearBarPointer=opcion;
+    notifyListeners();
+  }
+  //Valor del bar
+  void updatevalorBarPinter(valor){
+    valorBarPinter=valor;
+  }
+}
+
+
+//Chart
+class ChartData {
+  ChartData(this.x, this.y);
+  final String x;
+  final double? y;
 }
--- lib/view/custom_item/custom_item_view.dart
+++ lib/view/custom_item/custom_item_view.dart
@@ -1,65 +1,11 @@
-//TODO: Crear un dialogo que detecte que opcion habian selecionado añadir y personalice la creacion
+//TODO: Crear un widget que detecte que opcion habian selecionado añadir y personalice la creacion
 import 'package:flutter/material.dart';
 
-import 'package:stacked/stacked.dart';
-
-import '../dashboard/dashboard_viewmodel.dart';
-import 'alert_dialog_item_viewmodel.dart';
-
-class AlertDialogItemView extends StackedView<AlertDialogItemViewModel> {
-  const AlertDialogItemView(this.dashboardViewModel, {super.key});
-  final DashboardViewModel dashboardViewModel;
+class CustomItemView extends StatelessWidget {
+  const CustomItemView({super.key});
 
   @override
-  Widget builder(
-      BuildContext context, AlertDialogItemViewModel viewModel, Widget? child) {
-    return AlertDialog(
-      title: const Text('Selector de widget'),
-      content: Text("Hola"),
-      actions: [
-        TextButton(
-            child: const Text("Aceptar"),
-            onPressed: () {
-              switch (dashboardViewModel.widget) {
-                case "Qr":
-                  dashboardViewModel.addQr();
-                  break;
-                case "Gauge":
-                  dashboardViewModel.addGauge();
-                  break;
-                case "Cartesian":
-                  dashboardViewModel.addCartesianChart();
-                  break;
-                case "Circular":
-                  dashboardViewModel.addCircularChart();
-                  break;
-                case "Pyramid":
-                  dashboardViewModel.addPyramidChart();
-                  break;
-                case "Funnel":
-                  dashboardViewModel.addFunnelChart();
-                  break;
-                case "DateRangePicker":
-                  dashboardViewModel.addDateRangePicker();
-                  break;
-                case "RadialGauge":
-                  dashboardViewModel.addRadialGauge();
-                  break;
-                default:
-                  return;
-              }
-              Navigator.of(context).pop();
-            }),
-        TextButton(
-            child: const Text("Cancelar"),
-            onPressed: () {
-              Navigator.of(context).pop("Ninguno");
-            }),
-      ],
-    );
+  Widget build(BuildContext context) {
+    return Container();
   }
-
-  @override
-  AlertDialogItemViewModel viewModelBuilder(BuildContext context) =>
-      AlertDialogItemViewModel();
 }
